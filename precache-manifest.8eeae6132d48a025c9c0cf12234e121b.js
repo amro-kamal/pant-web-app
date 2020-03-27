a47a3d588cc585de312d6a32fe5d500a03cc35b9
amro-kamal <37993690+amro-kamal@users.noreply.github.com>
2020-03-27 03:27:40 +0300

    Updates

---
+++ precache-manifest.8eeae6132d48a025c9c0cf12234e121b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e317135f9039dce9db286347623f4593",
+    "url": "/pant-web-app/index.html"
+  },
+  {
+    "revision": "4860799dfc1b07f6d24f",
+    "url": "/pant-web-app/static/css/main.90b70eeb.chunk.css"
+  },
+  {
+    "revision": "b6ac1cf17d76e448cc62",
+    "url": "/pant-web-app/static/js/2.38348e94.chunk.js"
+  },
+  {
+    "revision": "4860799dfc1b07f6d24f",
+    "url": "/pant-web-app/static/js/main.e9094087.chunk.js"
+  },
+  {
+    "revision": "abcc975b76c2ceeac276",
+    "url": "/pant-web-app/static/js/runtime~main.c381e263.js"
+  },
+  {
+    "revision": "8b2e537a1f102d137d82d1caf019b17a",
+    "url": "/pant-web-app/static/media/app.8b2e537a.jpg"
+  },
+  {
+    "revision": "1952ab528aaebbeab80e493ff05f01a8",
+    "url": "/pant-web-app/static/media/background.1952ab52.jpg"
+  },
+  {
+    "revision": "1d4d819b838dc78c33373adaf17028aa",
+    "url": "/pant-web-app/static/media/bg1.1d4d819b.jpg"
+  },
+  {
+    "revision": "6d9099f18f69facdfcdbc18999144f14",
+    "url": "/pant-web-app/static/media/dimg.6d9099f1.jpeg"
+  },
+  {
+    "revision": "d5cc8cda8611842a991bb2a1011d2d07",
+    "url": "/pant-web-app/static/media/dimg2.d5cc8cda.jpg"
+  },
+  {
+    "revision": "8ad41cda3178b6b88c280089181a303a",
+    "url": "/pant-web-app/static/media/img1.8ad41cda.jpg"
+  },
+  {
+    "revision": "79fbb10126913b18dd13a9e5e32b643f",
+    "url": "/pant-web-app/static/media/img2.79fbb101.jpg"
+  },
+  {
+    "revision": "7404422d1d98c07601c0fea5dfde1314",
+    "url": "/pant-web-app/static/media/img3-.7404422d.jpg"
+  },
+  {
+    "revision": "cc158cf4c654555d35b99d97d6b9c49e",
+    "url": "/pant-web-app/static/media/logo.cc158cf4.png"
+  },
+  {
+    "revision": "04c212bd9f82b5c6dda1eef1cf75a1fd",
+    "url": "/pant-web-app/static/media/play.04c212bd.png"
+  }
+]);
